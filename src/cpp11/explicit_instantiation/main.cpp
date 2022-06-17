d1ee85fccb7a24c896b0fef3b34cb5eba1a184ac
yangbo1 <yangbo1@sensetime.com>
2022-06-17 10:21:50 +0800

    add main.cpp

--- src/cpp11/explicit_instantiation/main.cpp
+++ src/cpp11/explicit_instantiation/main.cpp
@@ -2,7 +2,7 @@
 #include "dog.hpp"
 using namespace std;
 
-extern template class Dog<float>;  // a
+//extern template class Dog<float>; 
 int main() {
     Dog<float> dog("wangcai", 0.72);
     cout << dog.getHeight() << endl;
